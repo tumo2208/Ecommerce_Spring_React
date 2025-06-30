d99a33aa743fc108116e831e995f89a204064688
tumo2208 <tuadc1234@gmail.com>
2025-06-30 14:50:14 +0700

    Start frontend implementing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import Navigation from "./Customer/Components/Navigation/Navigation.jsx";
 import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navigation></Navigation>
+
     </>
   )
 }
